74594eaeaf0feb7f068c6de7176e67e6f7578974
Kyle Rudolph <Kyle.Rudolph@outlook.com>
2020-08-09 23:06:22 -0500

    Hi

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,22 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
+            int age;
+            double height;
+            int weight;
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your weight: ");
+            weight = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height: ");
+            height = Convert.ToDouble(Console.ReadLine()); 
+            Console.WriteLine(name + " is " + age + " years old, his weight is " + weight +" kg and his height is " + height +"cm");
+
         }
     }
 }
